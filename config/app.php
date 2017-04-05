d4acb86eedfe1b7788cc67bf180c5ee4376a3d65
Jorge <jorge.garcia.soprasteria@gmail.com>
2017-04-05 13:20:56 +0200

    Added CORS support (every origin is allowed)

--- config/app.php
+++ config/app.php
@@ -180,6 +180,7 @@ return [
         App\Providers\RouteServiceProvider::class,
 
         Tymon\JWTAuth\Providers\JWTAuthServiceProvider::class,
+        Barryvdh\Cors\ServiceProvider::class,
     ],
 
     /*
