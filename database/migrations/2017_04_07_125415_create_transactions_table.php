022c53e61861f4ce8d632e07c616a0fb533e8cb9
Jorge <jorge.garcia.soprasteria@gmail.com>
2017-04-07 14:57:37 +0200

    Added migration class for creating transactions table.

---
+++ database/migrations/2017_04_07_125415_create_transactions_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTransactionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('concept');
+            $table->decimal('amount_source');
+            $table->decimal('amount_destination');
+            $table->string('currency_source');
+            $table->string('currency_destination');
+            $table->integer('state');
+            $table->integer('frequency');
+            $table->string('sms_custom_text')->nullable();
+            $table->string('ticket_otp')->nullable();
+            $table->integer('agent_destination');
+            $table->integer('agent_source');
+            $table->integer('user_id');
+            $table->timestamp('date_creation')->nullable();
+            $table->timestamp('date_start')->nullable();
+            $table->timestamp('date_end')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transactions');
+    }
+}
--- tests/Feature/TransactionsControllerTest.php
+++ tests/Feature/TransactionsControllerTest.php
@@ -1,7 +1,5 @@
 <?php
 
-namespace Tests\Feature;
-
 use Tests\BrowserKitTestCase;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithoutMiddleware;
@@ -19,5 +17,23 @@ class TransactionsControllerTest extends BrowserKitTestCase
     public function given_noAuthorization_When_getTransactions_Then_Returns401()
     {
         $this->get('/api/transactions')->seeStatusCode(401);
+    }
+
+    /**
+     * @test
+     * Test: GET: /api/transactions
+     */
+    public function given_authorizedUserWithoutTransactions_When_getTransactions_Then_ReturnsEmptyList() {
+
+        $user = factory(App\User::class)->create([
+            'document' => '123456789',
+            'doctype' => 'N',
+            'password' => bcrypt('foo')]);
+
+        $this->get('/api/transactions', $this->headers($user))
+            ->seeStatusCode(200)
+            ->seeJson([
+                "results" => []
+            ]);
     }
 }
