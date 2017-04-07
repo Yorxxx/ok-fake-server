6a79eb01343730be9ee0feb14c8692440e8ad2ea
Jorge <jorge.garcia.soprasteria@gmail.com>
2017-04-07 13:03:09 +0200

    Added currency conversion (hardcoded) between EUR and GBP.

--- app/Http/Controllers/CurrenciesController.php
+++ app/Http/Controllers/CurrenciesController.php
@@ -18,23 +18,28 @@ class CurrenciesController extends Controller
     public function getCurrency(Request $request) {
 
         if (!$request->has('currency_destination') || !$request->has('currency_source')) {
-            return $this->response->errorBadRequest();
+            return $this->response->errorBadRequest("Missing required params");
         }
-        return $this->response->errorBadRequest("Unknown currency");
 
-//        try {
-//            $token = JWTAuth::getToken();
-//            if (!$user = JWTAuth::toUser($token)) {
-//                return response()->json(['user_not_found'], 404);
-//            }
-//        } catch (\Tymon\JWTAuth\Exceptions\TokenExpiredException $e) {
-//            return response()->json(['token_expired'], $e->getStatusCode());
-//        } catch (\Tymon\JWTAuth\Exceptions\TokenInvalidException $e) {
-//            return response()->json(['token_invalid'], $e->getStatusCode());
-//        } catch (\Tymon\JWTAuth\Exceptions\JWTException $e) {
-//            return response()->json(['token_absent'], $e->getStatusCode());
-//        }
-        $settings = Setting::where('user_id', $user->id)->get();
-        return $this->collection($settings, new SettingsTranformer, ['key' => 'data']);
+        $allowed_currencies = ["EUR", "GBP"];
+
+        $dest = $request->get('currency_destination');
+        if (!in_array($dest, $allowed_currencies)) {
+            return $this->response->errorBadRequest("Unknown currency " . $dest);
+        }
+
+        $source = $request->get('currency_source');
+        if (!in_array($source, $allowed_currencies)) {
+            return $this->response->errorBadRequest("Unknown currency " . $source);
+        }
+
+        if (strcmp($source, "EUR") == 0 && strcmp($dest, "GBP") == 0) {
+            return "0.85";
+        }
+        if (strcmp($source, "GBP") == 0 && strcmp($dest, "EUR") == 0) {
+            return "1.17";
+        }
+
+        return "1";
     }
 }
