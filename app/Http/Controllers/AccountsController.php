2117ba4a2c9aa980aa16cf5ba7c6c511444e6ddf
Jorge <jorge.garcia.soprasteria@gmail.com>
2017-04-11 15:27:26 +0200

    [ADDED] AuthController for retrieving user from token.

--- app/Http/Controllers/AccountsController.php
+++ app/Http/Controllers/AccountsController.php
@@ -3,13 +3,10 @@
 namespace App\Http\Controllers;
 
 use App\Account;
-use App\User;
 use App\Transformers\AccountsTranformer;
-use Illuminate\Http\Request;
 use Dingo\Api\Routing\Helpers;
-use JWTAuth;
 
-class AccountsController extends Controller
+class AccountsController extends AuthController
 {
     use Helpers;
 
@@ -19,20 +16,7 @@ class AccountsController extends Controller
      * @Response(200, $accounts)
      */
     public function getAccounts() {
-        try {
-            $token = JWTAuth::getToken();
-            if (!$user = JWTAuth::toUser($token)) {
-                return response()->json(['user_not_found'], 404);
-            }
-        } catch (\Tymon\JWTAuth\Exceptions\TokenExpiredException $e) {
-            return response()->json(['token_expired'], $e->getStatusCode());
-        } catch (\Tymon\JWTAuth\Exceptions\TokenInvalidException $e) {
-            return response()->json(['token_invalid'], $e->getStatusCode());
-        } catch (\Tymon\JWTAuth\Exceptions\JWTException $e) {
-            return response()->json(['token_absent'], $e->getStatusCode());
-        }
-        $accounts = Account::where('user_id', $user->id)->get();
-        //return $this->response->array(['data' => $accounts], 200);
+        $accounts = Account::where('user_id', $this->getUserFromToken()->id)->get();
         return $this->collection($accounts, new AccountsTranformer);
     }
 
@@ -42,19 +26,7 @@ class AccountsController extends Controller
      * @Response(200)
      */
     public function link($id) {
-        try {
-            $token = JWTAuth::getToken();
-            if (!$user = JWTAuth::toUser($token)) {
-                return response()->json(['user_not_found'], 404);
-            }
-        } catch (\Tymon\JWTAuth\Exceptions\TokenExpiredException $e) {
-            return response()->json(['token_expired'], $e->getStatusCode());
-        } catch (\Tymon\JWTAuth\Exceptions\TokenInvalidException $e) {
-            return response()->json(['token_invalid'], $e->getStatusCode());
-        } catch (\Tymon\JWTAuth\Exceptions\JWTException $e) {
-            return response()->json(['token_absent'], $e->getStatusCode());
-        }
-        if (strcmp($id, $user->id) != 0)
+        if (strcmp($id, $this->getUserFromToken()->id) != 0)
             return $this->response->errorForbidden();
         return $this->response->accepted();
     }
