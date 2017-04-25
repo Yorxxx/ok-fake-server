a24fc24a42a49d1a71180796864a0a9b2fded761
Jorge <jorge.garcia.soprasteria@gmail.com>
2017-04-25 13:16:22 +0200

    [ADDED] TwilioMockClient for testing

---
+++ tests/Unit/Repositories/TwilioMockClient.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Tests\Unit\Repositories;
+
+/**
+ * Created by PhpStorm.
+ * User: jorge
+ * Date: 25/04/17
+ * Time: 11:46
+ */
+class TwilioMockClient
+{
+    public $messagesCalled = false;
+    public $messages;
+
+    /**
+     * TwilioMockClient constructor.
+     */
+    public function __construct()
+    {
+        $this->messages = new MessageClient();
+    }
+}
+
+class MessageClient {
+
+    public $createCalled = false;
+    public $phoneValue;
+    public $payload;
+
+    public function create(string $phone, array $payload) {
+        $this->createCalled = true;
+        $this->phoneValue = $phone;
+        $this->payload = $payload;
+    }
+}
